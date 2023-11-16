f160cd6cc116452899e9e85e39c4411e440e744a
Gilbert Röhrbein <payload.git@mailbox.org>
2023-11-16 19:46:04 +0100

    initial ParseExports

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::{env, path::Path};
+
+use oxc_allocator::Allocator;
+
+use oxc_parser::Parser;
+use oxc_span::SourceType;
+
+mod parse_exports;
+
+fn main() {
+    let name = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "test/test.ts".to_string());
+    let path = Path::new(&name);
+    let source_text = std::fs::read_to_string(path).unwrap_or_else(|_| panic!("{name} not found"));
+    let allocator = Allocator::default();
+    let source_type = SourceType::from_path(path).unwrap();
+    let ret = Parser::new(&allocator, &source_text, source_type).parse();
+
+    if !ret.errors.is_empty() {
+        for error in ret.errors {
+            let error = error.with_source_code(source_text.clone());
+            println!("{error:?}");
+        }
+        return;
+    }
+
+    let program = allocator.alloc(ret.program);
+    let exports = parse_exports::ParseExports::new().parse(program);
+
+    println!(
+        "export {{ {} }} from \"test/test.ts\"",
+        exports
+            .into_iter()
+            .map(|it| it.to_string())
+            .collect::<Vec<_>>()
+            .join(", ")
+    );
+}
---
+++ src/parse_exports.rs
@@ -0,0 +1,116 @@
+use std::{fmt::Display, marker::PhantomData};
+
+use oxc_ast::{ast::*, Visit};
+
+use oxc_span::Atom;
+
+pub struct ParseExports<'a> {
+    lifetime: PhantomData<&'a ()>,
+    exports: Vec<Export>,
+}
+
+impl<'a> ParseExports<'a> {
+    pub fn new() -> Self {
+        Self {
+            lifetime: PhantomData,
+            exports: Vec::new(),
+        }
+    }
+
+    pub fn parse(mut self, input: &'a Program<'a>) -> Vec<Export> {
+        self.visit_program(input);
+        self.exports
+    }
+}
+
+pub enum Export {
+    Name(Atom),
+    Wildcard,
+    Default,
+}
+
+impl Display for Export {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Name(atom) => atom.fmt(f),
+            Self::Wildcard => f.write_str("*"),
+            Self::Default => f.write_str("default"),
+        }
+    }
+}
+
+impl<'a> Visit<'a> for ParseExports<'a> {
+    fn visit_export_named_declaration(&mut self, decl: &'a ExportNamedDeclaration<'a>) {
+        if decl.declaration.is_some() && !decl.specifiers.is_empty() {
+            todo!("conflict 1");
+        }
+        if let Some(ref it) = decl.declaration {
+            match it {
+                Declaration::VariableDeclaration(it) => {
+                    for name in names_of_variable_decl(it) {
+                        self.exports.push(Export::Name(name));
+                    }
+                }
+                it => {
+                    if let Some(name) = name_of_single_decl(it) {
+                        self.exports.push(Export::Name(name));
+                    }
+                }
+            }
+        }
+        for spec in decl.specifiers.iter() {
+            let name = spec.exported.name().clone();
+            self.exports.push(Export::Name(name));
+        }
+    }
+
+    fn visit_export_default_declaration(&mut self, _decl: &'a ExportDefaultDeclaration<'a>) {
+        self.exports.push(Export::Default);
+    }
+
+    fn visit_export_all_declaration(&mut self, _decl: &'a ExportAllDeclaration<'a>) {
+        self.exports.push(Export::Wildcard);
+    }
+}
+
+fn name_of_single_decl(it: &Declaration) -> Option<Atom> {
+    match it {
+        Declaration::FunctionDeclaration(it) => name_of_some_binding(it.id.as_ref()),
+        Declaration::ClassDeclaration(it) => name_of_some_binding(it.id.as_ref()),
+        Declaration::TSTypeAliasDeclaration(it) => Some(it.id.name.clone()),
+        Declaration::TSInterfaceDeclaration(it) => Some(it.id.name.clone()),
+        Declaration::TSEnumDeclaration(it) => Some(it.id.name.clone()),
+        Declaration::TSModuleDeclaration(it) => Some(it.id.name().clone()),
+        Declaration::TSImportEqualsDeclaration(it) => Some(it.id.name.clone()),
+        Declaration::VariableDeclaration(_) => unimplemented!("use names_of_variable_decl"),
+    }
+}
+
+fn name_of_some_binding(it: Option<&BindingIdentifier>) -> Option<Atom> {
+    it.map(|it| it.name.clone())
+}
+
+fn names_of_variable_decl(it: &VariableDeclaration) -> Vec<Atom> {
+    it.declarations
+        .iter()
+        .flat_map(|it| names_of_binding_pattern_kind(&it.id.kind))
+        .collect()
+}
+
+fn names_of_binding_pattern_kind(it: &BindingPatternKind) -> Vec<Atom> {
+    match it {
+        BindingPatternKind::BindingIdentifier(it) => vec![it.name.clone()],
+        BindingPatternKind::ObjectPattern(it) => it
+            .properties
+            .iter()
+            .flat_map(|it| names_of_binding_pattern_kind(&it.value.kind))
+            .collect(),
+        BindingPatternKind::ArrayPattern(it) => it
+            .elements
+            .iter()
+            .flatten()
+            .flat_map(|it| names_of_binding_pattern_kind(&it.kind))
+            .collect(),
+        BindingPatternKind::AssignmentPattern(it) => names_of_binding_pattern_kind(&it.left.kind),
+    }
+}
